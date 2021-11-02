278e6ee7bd9ba7bc40e672bcb8b4dde0a7982282
Gabriela <gabrieladeoliv@gmail.com>
2021-11-02 16:13:04 -0300

    Variables

---
+++ Udacity - Intro to Python Programming/Delaware.py
@@ -0,0 +1,22 @@
+'''
+Say you wanted to create a variable for Delaware and assign it a value to signify that it joined the Union first. 
+Which of the following are valid variable names and assignments in Python?
+del = 1
+delaware = 1
+1 de = first
+de = 1
+'''
+
+del = 1 #Palavra reservada del não pode ser nome de variável
+print (del) 
+
+delaware = 1
+print(delaware) #Correto
+
+
+1 de = first #Não pode ter espaço em nome de variável
+print (1 de)
+
+
+de = 1
+print(de) #Correto
---
+++ Udacity - Intro to Python Programming/changing_variables_values.py
@@ -0,0 +1,17 @@
+'''
+carrots = 24
+rabbits = 8
+crs_per_rab = carrots/rabbits
+rabbits =12 #Esse valor não interfere
+
+print(crs_per_rab)
+'''
+
+
+#Necessário rodar novamente o comando crs_per_rab = carrots/rabbits
+carrots = 24
+rabbits = 8
+crs_per_rab = carrots/rabbits
+rabbits =12 
+crs_per_rab = carrots/rabbits
+print(crs_per_rab)
