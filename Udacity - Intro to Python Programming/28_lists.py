848c129a0c90e05f4ad603cc5dadd00e7c478f74
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2021-11-18 20:56:57 -0300

    list: Stock exchange example

---
+++ Udacity - Intro to Python Programming/28_lists.py
@@ -0,0 +1,7 @@
+VINIX = ['C', 'MA', 'BA', 'PG', 'CSCO', 'VZ', 'PFE', 'HD', 'INTC', 'T', 'V', 'UNH', 'WFC', 'CVX', 'BAC', 'JNJ', 'GOOGL', 'GOOG', 'BRK.B', 'XOM', 'JPM', 'FB', 'AMZN', 'MSFT', 'AAPL']
+
+print(VINIX[0])
+print(VINIX[1])
+
+print('GE' in VINIX)
+print('GOOGL' in VINIX)
