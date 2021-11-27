2365eeb523e04e7514138d4c2c361e296fe1d2cf
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2021-11-27 18:41:54 -0300

    Complex Boolean Expressions

---
+++ Udacity - Intro to Python Programming/57_boolean_expressions_conditions.py
@@ -0,0 +1,21 @@
+weight = 55
+height = 164
+is_raining = True
+is_sunny = True
+
+
+if 18.5 <= weight / height**2 < 25:
+    print("BMI is considered 'normal'")
+
+if is_raining and is_sunny:
+    print("Is there a rainbow?")
+'''
+if 18.5 <= weight / height**2 < 25:
+    print("BMI is considered 'normal'")
+
+if is_raining and is_sunny:
+    print("Is there a rainbow?")
+
+if (not unsubscribed) and (location == "USA" or location == "CAN"):
+    print("send email")
+'''
