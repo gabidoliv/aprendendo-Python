e07e634b7ee9d27663c65ef919a6a52d1800bed5
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2021-12-05 12:05:45 -0300

    Lists - range

---
+++ Udacity - Intro to Python Programming/70_range.py
@@ -0,0 +1,11 @@
+print(list(range(4))) # [0, 1, 2, 3]
+
+print(list(range(4,8))) #[4, 5, 6, 7]
+
+print(list(range(4,10,2))) # [4, 6, 8]
+
+print(list(range(0,-5))) # []
+
+print(list(range(-5))) # []
+
+print(list(range(0,-10,-2))) # [0, -2, -4, -6, -8]
