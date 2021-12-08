4b6b3f9081c14ae523b2d2b5eb51bf013dc8d0b2
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2021-12-07 22:14:44 -0300

    Building dictionaries

---
+++ Udacity - Intro to Python Programming/72_dictionaries.py
@@ -0,0 +1,10 @@
+book_title =  ['great', 'expectations','the', 'adventures', 'of', 'sherlock','holmes','the','great','gasby','hamlet','adventures','of','huckleberry','fin']
+
+word_counter = {} #Cria dicionário vazio
+
+for word in book_title:
+    if word not in word_counter:
+        word_counter[word] = 1
+    else:
+        word_counter[word] += 1
+print (word_counter)
