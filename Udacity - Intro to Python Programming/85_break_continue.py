0ba234953fd9b403a8a6530a9b0e71104ac2fbfb
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2021-12-24 18:53:03 -0300

    Break - Continue

---
+++ Udacity - Intro to Python Programming/85_break_continue.py
@@ -0,0 +1,21 @@
+# break - finaliza o loop
+# continue - pula uma iteração do loop
+
+manifest = [("bananas", 15), ("mattresses", 24), ("dog kennels", 42), ("machine", 120), ("cheeses", 5)]
+
+# the code breaks the loop when weight exceeds or reaches the limit
+print("METHOD 1")
+weight = 0
+items = []
+for cargo_name, cargo_weight in manifest:
+    print("current weight: {}".format(weight))
+    if weight >= 100:
+        print("  breaking loop now!")
+        break
+    else:
+        print("  adding {} ({})".format(cargo_name, cargo_weight))
+        items.append(cargo_name)
+        weight += cargo_weight
+
+print("\nFinal Weight: {}".format(weight))
+print("Final Items: {}".format(items))
