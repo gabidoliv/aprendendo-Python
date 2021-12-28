88ac97a0ea3d2816bc417957e5bd9c0b0990b4ae
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2021-12-27 22:21:44 -0300

    quiz prime numbers

---
+++ Udacity - Intro to Python Programming/88_loops_quiz_prime_numbers.py
@@ -0,0 +1,5 @@
+## Your code should check if each number in the list is a prime number
+check_prime = [26, 39, 51, 53, 57, 79, 85]
+
+## write your code here
+## HINT: You can use the modulo operator to find a factor
