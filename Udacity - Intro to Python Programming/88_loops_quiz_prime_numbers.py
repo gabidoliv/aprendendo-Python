2dfeb1a59c9ecb960dc8011fec6364bfbac5f3d8
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2021-12-27 22:38:26 -0300

    quiz prime numbers solution

--- Udacity - Intro to Python Programming/88_loops_quiz_prime_numbers.py
+++ Udacity - Intro to Python Programming/88_loops_quiz_prime_numbers.py
@@ -1,5 +1,12 @@
 ## Your code should check if each number in the list is a prime number
 check_prime = [26, 39, 51, 53, 57, 79, 85]
-
-## write your code here
-## HINT: You can use the modulo operator to find a factor
+#iterar na lista
+for num in check_prime: 
+    for i in range(2,num):
+        #Um número não é primo se o resto de divisão é zero
+        if (num % i) == 0:
+            print("{} não é primo porque {} é um fator de {}".format(num,i,num))
+            break
+        # Caso contrário, continuar checando até buscar todos os possíveis fatores e só então, declarar como primo
+        if i == num -1:
+            print("{} é um número primo".format(num))
