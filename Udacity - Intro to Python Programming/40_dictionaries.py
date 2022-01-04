88c9d272c7ead4aef3f7c649dccb9b714d496518
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2022-01-04 12:23:27 -0300

    dicionaries + practice control flow

--- Udacity - Intro to Python Programming/40_dictionaries.py
+++ Udacity - Intro to Python Programming/40_dictionaries.py
@@ -10,11 +10,25 @@ print(elements["helium"])  # mostra o valor mapeado para "helium"
 elements["lithium"] = 3  # insere "lithium" com valor 3 no dicionário
 print(elements)
 
-print("carbon" in elements)
-print(elements.get("dilithium")) 
+print("carbon in elements? ", "carbon" in elements)
+print("dilithium in elements? ", elements.get("dilithium")) 
 # Se é esperado que as buscas falhem às vezes, get pode ser uma escolha melhor que colchetes porque erros podem travar o programa (crash)
 
 #Operadores de identidade (são diferentes de operadores de comparação)
 n = elements.get("dilithium")
-print(n is None)
-print(n is not None)
+print("dilithium not in elements? ", n is None) # Verifica se n é igual a None - retorna True
+print("dilithium in elements? ", n is not None) # Verifica se n não é igual a None - retorna False
+
+'''
+clear()	- Removes all the elements from the dictionary
+copy()	- Returns a copy of the dictionary
+fromkeys()	- Returns a dictionary with the specified keys and value
+get()	- Returns the value of the specified key
+items()	- Returns a list containing a tuple for each key value pair
+keys()	- Returns a list containing the dictionary's keys
+pop()	- Removes the element with the specified key
+popitem()	- Removes the last inserted key-value pair
+setdefault()	- Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+update()	- Updates the dictionary with the specified key-value pairs
+values()	- Returns a list of all the values in the dictionary
+'''
--- Udacity - Intro to Python Programming/72_dictionaries.py
+++ Udacity - Intro to Python Programming/72_dictionaries.py
@@ -4,7 +4,7 @@ word_counter = {} #Cria dicionário vazio
 
 for word in book_title:
     if word not in word_counter:
-        word_counter[word] = 1 # Se já existe o elemento na lista soma 1 ao valor dele
+        word_counter[word] = 1 # Se não existe, seu valor passa a ser 1 
     else:
-        word_counter[word] += 1 #Se não existe, seu valor passa a ser 1
+        word_counter[word] += 1 # Se já existe o elemento na lista soma 1 ao valor dele
 print (word_counter)
