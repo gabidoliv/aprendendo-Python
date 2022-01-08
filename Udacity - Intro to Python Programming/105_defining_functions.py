b445c872bd0dc4696e77977b93e0d80a679cb273
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2022-01-07 22:11:34 -0300

    Defining functions lesson

---
+++ Udacity - Intro to Python Programming/105_defining_functions.py
@@ -0,0 +1,20 @@
+# Funções possibilitam encapsular tarefas
+# Encapsular - realizar um série de etapas com um comando simples
+# Sintaxe - nome_da_função (argumentos separados por vírgula se existirem): corpo da função
+
+# Exemplo de função sem argumentos 
+def print_greeting():
+    print('Hello World!')
+
+# Exemplo de função com argumentos e corpo indentado
+def cylinder_volume(height,radius):
+    pi = 3.14159 # variável local
+    return height * pi * radiuss ** 2 # Retorna o valor de saída quando a função é chamada
+
+# Algumas funções (print por exemplo) não retornam nada
+# Print exibe texto na janela de saída, o valor retornado é nulo
+return_value = print("I wish to register a complaint.")
+print("Output: ", return_value) # None - padrão de saída de uma função, se não houver algo explicitado em return
+
+# Print fornece saída para o console
+# Return fornece o valor com o qual você pode armazenar, trabalhar e programar mais tarde
